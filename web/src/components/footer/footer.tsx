37d8ecf34aaa56bd23983cc88f1f787e706fecce
mfranciele <mariafrancielepires@gmail.com>
2024-11-07 13:26:15 -0300

    style: CSS simples, estilização dos componentes de forma parcial

--- web/src/components/footer/footer.tsx
+++ web/src/components/footer/footer.tsx
@@ -1,11 +1,11 @@
 import './footer.css';
 
-const Footer = () => {
+function Footer() {
   return (
     <footer className="footer">
       <p>&copy; 2024 Application TG.</p>
     </footer>
   );
-};
+}
 
 export default Footer;
--- web/src/components/header/header.tsx
+++ web/src/components/header/header.tsx
@@ -1,16 +1,47 @@
+import { useState } from 'react';
 import './header.css';
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false); 
+
+  const handleOpenModal = (event: any) => {
+    event.preventDefault(); 
+    setShowModal(true);  
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false); 
+  };
+
+  const handleOutsideClick = (event: any) => {
+    if (event.target.classList.contains('modal')) {
+      handleCloseModal();
+    }
+  };
+  
   return (
     <header className="header">
       <h1>Application TG</h1>
       <nav>
         <ul>
-          <li><a href="/about">Sobre</a></li>
-          <li><a href="/contact">Docs</a></li>
-          <li><a href="https://github.com/MFranciele?tab=repositories">GitHub</a></li>
+          <li><a href="/about" onClick={handleOpenModal}>Sobre</a></li>
+          <li><a href="https://github.com/MFranciele?tab=repositories">Docs</a></li>
         </ul>
       </nav>
+      {showModal && (
+        <div className="modal" onClick={handleOutsideClick}>
+          <div className="modal-content">
+            <h2>Sobre</h2>
+            <p>Essa aplicação tem como finalidade contribuir no estudo para 
+              avaliar a eficácia do SIEM Graylog no ambiente de nuvem Azure para 
+              detectar e auxiliar na resposta de incidentes cibernéticos, 
+              utilizando o framework MITRE ATTACK como base de conhecimento. 
+              Investigando como a ferramenta de SIEM do Graylog faz 
+              aproveitamento do ambiente em nuvem para correlacionar e 
+              visibilizar os eventos de segurança.</p>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
