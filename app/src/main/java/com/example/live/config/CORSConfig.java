e8d298a2596989a70e34395a7a15517bec9f5b79
mfranciele <mariafrancielepires@gmail.com>
2024-11-10 17:36:56 -0300

    up

--- app/src/main/java/com/example/live/config/CORSConfig.java
+++ app/src/main/java/com/example/live/config/CORSConfig.java
@@ -2,27 +2,23 @@ package com.example.live.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CORSConfig {
 
-    private String url = "*";
-    
     @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowCredentials(true); 
-        config.addAllowedOrigin(this.url); 
-        config.addAllowedHeader(this.url);
-        config.addAllowedMethod(this.url);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source); 
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedHeaders("*")
+                    .allowedMethods("*");
+            }
+        };
     }
 }
